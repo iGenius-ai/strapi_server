e67beebf06ac0ebc82ad745aec7d03169f58d4d1
Charles Emmanuel <relate2hazel@gmail.com>
2023-09-06 23:01:19 +0100

    Strapi Fix

--- config/env/production/database.js
+++ config/env/production/database.js
@@ -3,7 +3,7 @@ module.exports = ({ env }) => ({
     client: 'postgres',
     connection: {
       host: env('PGHOST', '127.0.0.1'),
-      port: env.int('PGPORT', 5931),
+      port: env.int('PGPORT', 5432),
       database: env('PGDATABASE', 'railway'),
       user: env('PGUSER', 'postgres'),
       password: env('PGPASSWORD', 'password'),
